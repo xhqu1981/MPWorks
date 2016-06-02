5413b2a117270a39791029f2986003fc8ad607dd
Xiaohui Qu <xqu@lbl.gov>
2016-06-01 22:13:23 -0700

    refactor FW creation to functions

--- mpworks/workflows/snl_to_wf_nmr.py
+++ mpworks/workflows/snl_to_wf_nmr.py
@@ -5,6 +5,7 @@ from fireworks.core.firework import Tracker
 from fireworks.utilities.fw_utilities import get_slug
 from pymatgen import Composition
 
+from mpworks.dupefinders.dupefinder_vasp import DupeFinderDB
 from mpworks.firetasks.custodian_task import get_custodian_task
 from mpworks.firetasks.nmr_tasks import snl_to_nmr_spec
 from mpworks.firetasks.snl_tasks import AddSNLTask
@@ -24,6 +25,33 @@ __date__ = 'May 31, 2016'
 This is modified from Wei Chen's snl_to_wf_elastic.
 """
 
+def get_nmr_vasp_fw(fwid, copy_contcar, istep, nick_name, parameters, priority, snl):
+    spec = snl_to_nmr_spec(snl, istep, parameters)
+    trackers = [Tracker('FW_job.out'), Tracker('FW_job.error'), Tracker('vasp.out'), Tracker('OUTCAR'),
+                Tracker('OSZICAR'), Tracker('OUTCAR.relax1'), Tracker('OUTCAR.relax2')]
+    spec['_priority'] = priority
+    spec['_queueadapter'] = QA_VASP
+    spec['_trackers'] = trackers
+    tasks = [VaspWriterTask()]
+    if copy_contcar:
+        parameters["use_CONTCAR"] = True
+        parameters["files"] = "CONTCAR"
+        parameters["keep_velocities"] = False
+        tasks.append(VaspCopyTask(parameters=parameters))
+    tasks.append(get_custodian_task(spec))
+    vasp_fw = Firework(tasks, spec, name=get_slug(nick_name + '--' + spec['task_type']),
+                       fw_id=fwid)
+    return vasp_fw
+
+def get_nmr_db_fw(nick_name, fwid, prev_task_type, priority, task_class):
+    trackers_db = [Tracker('FW_job.out'), Tracker('FW_job.error')]
+    spec = {'task_type': 'VASP db insertion', '_priority': priority * 2,
+            '_allow_fizzled_parents': True, '_queueadapter': QA_DB, "_dupefinder": DupeFinderDB().to_dict(),
+            '_trackers': trackers_db}
+    db_fw = Firework([task_class()], spec, name=get_slug(nick_name + '--' + spec['task_type'] +
+                                                         '--' + prev_task_type),
+                     fw_id=fwid)
+    return db_fw
 
 def snl_to_wf_elastic(snl, parameters):
     # parameters["user_vasp_settings"] specifies user defined incar/kpoints parameters
@@ -53,55 +81,64 @@ def snl_to_wf_elastic(snl, parameters):
 
     parameters["exact_structure"] = True
     # run Triple Jump Structure Relaxation to Converge to a Very Small Force
-    geom_fwid = None
-    db_fwid = None
+    geom_calc_fwid = None
+    geom_db_fwid = None
     for istep in [1, 2, 3]:
-        spec = snl_to_nmr_spec(snl, istep, parameters)
-        trackers = [Tracker('FW_job.out'), Tracker('FW_job.error'), Tracker('vasp.out'), Tracker('OUTCAR'),
-                    Tracker('OSZICAR'), Tracker('OUTCAR.relax1'), Tracker('OUTCAR.relax2')]
-        trackers_db = [Tracker('FW_job.out'), Tracker('FW_job.error')]
-        # run GGA structure optimization
-        spec['_priority'] = priority
-        spec['_queueadapter'] = QA_VASP
-        spec['_trackers'] = trackers
-        tasks = [VaspWriterTask()]
-        if istep >= 2:
-            parameters["use_CONTCAR"] = True
-            parameters["files"] = "CONTCAR"
-            parameters["keep_velocities"] = False
-            tasks.append(VaspCopyTask(parameters=parameters))
-        tasks.append(get_custodian_task(spec))
-        geom_fwid = cur_fwid
+        # Geometry Optimization
+        copy_contcar = istep >= 2
+        geom_calc_fwid = cur_fwid
         cur_fwid += 1
-        fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']),
-                            fw_id=geom_fwid))
-        geom_task_type = spec['task_type']
-
-        # insert into DB - GGA structure optimization
-        spec = {'task_type': 'VASP db insertion', '_priority': priority * 2,
-                '_allow_fizzled_parents': True, '_queueadapter': QA_DB, "_dupefinder": DupeFinderDB().to_dict(),
-                '_trackers': trackers_db}
-        db_fwid = cur_fwid
-        cur_fwid += 1
-        fws.append(
-            Firework([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']
-                                                           + '--' + geom_task_type),
-                     fw_id=db_fwid))
-        connections[geom_fwid] = [db_fwid]
+        vasp_fw = get_nmr_vasp_fw(geom_calc_fwid, copy_contcar, istep, nick_name, parameters, priority, snl)
+        fws.append(vasp_fw)
+        geom_task_type = vasp_fw.spec['task_type']
         if istep == 1:
-            connections[addsnl_fwid] = [geom_fwid]
+            connections[addsnl_fwid] = [geom_calc_fwid]
+        else:
+            prev_db_fwid = geom_db_fwid
+            connections[prev_db_fwid] = [geom_calc_fwid]
 
-    spec = {'task_type': 'Setup Deformed Struct Task', '_priority': priority,
-                '_queueadapter': QA_CONTROL}
-    fws.append(Firework([SetupDeformedStructTask()], spec, 
-                        name=get_slug(f + '--' + spec['task_type']),fw_id=3))
-    connections[2] = [3]
+        # insert into DB
+        task_class =  VaspToDBTask
+        prev_task_type = geom_task_type
 
-    wf_meta = get_meta_from_structure(snl.structure)
-    wf_meta['run_version'] = 'May 2013 (1)'
+        geom_db_fwid = cur_fwid
+        cur_fwid += 1
+        db_fw = get_nmr_db_fw(nick_name, geom_db_fwid, prev_task_type, priority, task_class)
+        fws.append(db_fw)
+        connections[geom_calc_fwid] = [geom_db_fwid]
 
-    if '_materialsproject' in snl.data and 'submission_id' in snl.data['_materialsproject']:
-        wf_meta['submission_id'] = snl.data['_materialsproject']['submission_id']
+
+    # Calculate NMR Tensors
+    for istep in [-1, -2]:
+        # -1: Chemical Shift, -2: EFG
+        # Geometry Optimization
+        copy_contcar = istep >= 2
+        geom_calc_fwid = cur_fwid
+        cur_fwid += 1
+        vasp_fw = get_nmr_vasp_fw(geom_calc_fwid, copy_contcar, istep, nick_name, parameters, priority, snl)
+        fws.append(vasp_fw)
+        geom_task_type = vasp_fw.spec['task_type']
+        if istep == 1:
+            connections[addsnl_fwid] = [geom_calc_fwid]
+        else:
+            prev_db_fwid = geom_db_fwid
+            connections[prev_db_fwid] = [geom_calc_fwid]
+
+        # insert into DB
+        task_class = VaspToDBTask
+        prev_task_type = geom_task_type
+
+        geom_db_fwid = cur_fwid
+        cur_fwid += 1
+        db_fw = get_nmr_db_fw(nick_name, geom_db_fwid, prev_task_type, priority, task_class)
+        fws.append(db_fw)
+        connections[geom_calc_fwid] = [geom_db_fwid]
 
     return Workflow(fws, connections, name=Composition(
         snl.structure.composition.reduced_formula).alphabetical_formula, metadata=wf_meta)
+
+
+
+
+
+
