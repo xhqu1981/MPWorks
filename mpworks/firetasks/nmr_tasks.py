3aed2f88f4a910833b8d77d5813d423ba8748dfb
Xiaohui Qu <xqu@berkeley.edu>
2017-05-29 14:51:27 -0700

    fix file name

--- mpworks/firetasks/nmr_tasks.py
+++ mpworks/firetasks/nmr_tasks.py
@@ -283,12 +283,17 @@ class NmrVaspToDBTask(VaspToDBTask):
             for k in ['chemical_shifts', 'manual_kpt_average', 'rmsd',
                       'rmsd_header', 'manual_kpt_data']:
                 nmr_fields = fw_spec[k]
-            shutil.copytree(prev_dir, "fake_nmr_vasp_files")
-            fake_prev_dir = os.path.abspath("fake_nmr_vasp_files")
+            sub_dir_name = "fake_nmr_vasp_files"
+            shutil.copytree(prev_dir, sub_dir_name)
+            for fn in ["CHGCAR", "CHGCAR.gz"]:
+                ffn = os.path.join(sub_dir_name, fn)
+                if os.path.exists(ffn):
+                    os.remove(ffn)
+            fake_prev_dir = os.path.abspath(sub_dir_name)
             fw_spec['prev_vasp_dir'] = fake_prev_dir
             with zopen(zpath(os.path.join(fake_prev_dir, 'FW.json')), 'rt') as f:
                 fw_dict = json.load(f)
-            fw_dict["prev_task_type"] = "NMR CS"
+            fw_dict["task_type"] = "NMR CS"
             with zopen(zpath(os.path.join(fake_prev_dir, 'FW.json')), 'wt') as f:
                 json.dump(fw_dict, f, sort_keys=True, indent=4)
         elif prev_task_type == "NMR EFG":
